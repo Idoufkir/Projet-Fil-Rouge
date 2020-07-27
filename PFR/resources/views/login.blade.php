ad7a2e557e836ce5d08d6c67d82df04c728de11a
Idoufkir <57219106+Idoufkir@users.noreply.github.com>
2020-07-27 09:48:28 +0100

    update

---
+++ PFR/resources/views/login.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>INSPINIA | Login</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body class="gray-bg">
+
+    <div class="middle-box text-center loginscreen animated fadeInDown">
+        <div>
+            <div>
+
+                <a href="/"><h1 class="logo-name"></h1></a>
+
+
+            </div>
+            <h3>Welcome to Univers CRM</h3>
+            <p>Login in. To see it in action.</p>
+            <form class="m-t" role="form" action="index.html">
+                <div class="form-group">
+                    <input type="email" class="form-control" placeholder="E-mail" required="">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Password" required="">
+                </div>
+                <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
+
+                <a href="#"><small>Forgot password?</small></a>
+                <p class="text-muted text-center"><small>Do not have an account?</small></p>
+                <a class="btn btn-sm btn-white btn-block" href="/register">Create an account</a>
+            </form>
+            <div class="col-lg-12 col-lg-offset-2 text-center m-t-lg m-b-lg">
+                <p class="ibox-footer"><strong>&copy; <script>document.write(new Date().getFullYear())</script> Universe CRM</strong><br/> PFR de la Promotion 2019 du référentiel développement web | YouCode Safi.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
